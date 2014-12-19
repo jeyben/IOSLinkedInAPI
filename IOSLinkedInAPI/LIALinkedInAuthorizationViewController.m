9bc032a6ad70f275495f08e69d6ea0facbbac2c4
Robert Cantoni <bob@nicemohawk.com>
2014-12-19 10:10:47 -0500

    Moving -super call to first line of -viewDidAppear:

--- IOSLinkedInAPI/LIALinkedInAuthorizationViewController.m
+++ IOSLinkedInAPI/LIALinkedInAuthorizationViewController.m
@@ -72,10 +72,10 @@ BOOL handlingRedirectURL;
 }
 
 - (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
     NSString *linkedIn = [NSString stringWithFormat:@"https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=%@&scope=%@&state=%@&redirect_uri=%@", self.application.clientId, self.application.grantedAccessString, self.application.state, [self.application.redirectURL LIAEncode]];
     [self.authenticationWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:linkedIn]]];
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
-    [super viewDidAppear:animated];
 }
 
 - (void)viewWillLayoutSubviews {
