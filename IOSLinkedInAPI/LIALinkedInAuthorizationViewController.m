a8a2eed19124981ffe3ff65068de9c9dec868425
Francisco Montoro <Francisco.Montoro@jobsite.co.uk>
2016-08-22 15:08:32 +0100

    Updated authentication endpoint which was causing an error when trying to authenticate (returned state had some extra chars at the end)

--- IOSLinkedInAPI/LIALinkedInAuthorizationViewController.m
+++ IOSLinkedInAPI/LIALinkedInAuthorizationViewController.m
@@ -73,7 +73,7 @@ BOOL handlingRedirectURL;
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    NSString *linkedIn = [NSString stringWithFormat:@"https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=%@&scope=%@&state=%@&redirect_uri=%@", self.application.clientId, self.application.grantedAccessString, self.application.state, [self.application.redirectURL LIAEncode]];
+    NSString *linkedIn = [NSString stringWithFormat:@"https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=%@&scope=%@&state=%@&redirect_uri=%@", self.application.clientId, self.application.grantedAccessString, self.application.state, [self.application.redirectURL LIAEncode]];
     [self.authenticationWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:linkedIn]]];
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
 }
